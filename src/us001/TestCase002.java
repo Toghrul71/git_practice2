73daab0c758da827a75b0e20b2e437bc8cf253bc
Toghrul <te7999@gmail.com>
2022-01-11 11:06:55 -0800

    added test case 004

--- src/us001/TestCase002.java
+++ src/us001/TestCase002.java
@@ -6,4 +6,5 @@ public class TestCase002 {
     // step 3
     // 1.commit means to send changes into local git (my computer)
     // 2. push means to further send those commited changes to remote github
+    // 3. gitignore to not send files to remote
 }
